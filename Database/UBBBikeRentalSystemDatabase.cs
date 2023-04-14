28f0ae441eb6bd2cef5a0a16350b4ce0456cd900
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-04-14 16:49:47 +0200

    override DbSet<User>(CS0114)

--- Database/UBBBikeRentalSystemDatabase.cs
+++ Database/UBBBikeRentalSystemDatabase.cs
@@ -4,12 +4,12 @@ using Microsoft.EntityFrameworkCore;
 using UBBBikeRentalSystem.Models;
 
 namespace UBBBikeRentalSystem.Database {
-    public class UBBBikeRentalSystemDatabase : IdentityDbContext<Models.User, IdentityRole<int>, int> {
-        public DbSet<Models.Vehicle> Vehicles { get; set; }
-        public DbSet<Models.Reservation> Reservations { get; set; }
-        public DbSet<Models.User> Users { get; set; }
-        public DbSet<Models.ReservationPoint> ReservationPoints { get; set; }
-        public DbSet<Models.VehicleType> VehicleTypes { get; set; }
+    public class UBBBikeRentalSystemDatabase : IdentityDbContext<User, IdentityRole<int>, int> {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public override DbSet<User> Users { get; set; }
+        public DbSet<ReservationPoint> ReservationPoints { get; set; }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseInMemoryDatabase("UBBBikeRentalSystemDatabase");
