668ae780a80c38508ff9db580a3c658a67dbef01
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-05-24 14:05:31 +0200

    Users index template

--- Areas/Users/Controllers/UsersController.cs
+++ Areas/Users/Controllers/UsersController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Data;
+using UBBBikeRentalSystem.Models;
+using UBBBikeRentalSystem.Services;
+using UBBBikeRentalSystem.ViewModels;
 
 namespace UBBBikeRentalSystem.Areas.Users.Controllers {
     [Area("Users")]
     public class UsersController : Controller {
-        public IActionResult Index() {
-            return View();
+        private readonly IRepository<User, string> _userRepository;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(IRepository<User, string> uDB, UserManager<User> userManager, IMapper mapper) {
+            _userRepository = uDB;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        private async Task<User?> GetLoggedInUser() {
+            if (User?.Identity?.IsAuthenticated ?? false) {
+                try {
+                    return await _userManager.FindByNameAsync(User?.Identity?.Name ?? "");
+                } catch {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() {
+            User? loggedInUser = await GetLoggedInUser();
+            if (loggedInUser == null) return Forbid();
+            UserViewModel userVM = _mapper.Map<UserViewModel>(loggedInUser);
+
+            return View(userVM);
         }
     }
 }
