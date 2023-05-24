76428036a9c86bc02b1b0e58350d37f3beb3719b
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-05-24 13:46:07 +0200

    Poprawienie kontrolera Users

--- Areas/Users/Controllers/UsersController.cs
+++ Areas/Users/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace UBBBikeRentalSystem.Areas.Users.Controllers {
+    [Area("Users")]
     public class UsersController : Controller {
         public IActionResult Index() {
             return View();
