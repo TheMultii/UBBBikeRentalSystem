ad92369c83c5ba3de5f448ec1913c68cbf9238b3
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-05-29 19:02:48 +0200

    Usunięcie niepotrzebnego importu

--- Areas/Admin/ViewModels/POST_AreaAdminEditUserReservationViewModel.cs
+++ Areas/Admin/ViewModels/POST_AreaAdminEditUserReservationViewModel.cs
@@ -1,6 +1,4 @@
-﻿using UBBBikeRentalSystem.ViewModels;
-
-namespace UBBBikeRentalSystem.Areas.Admin.ViewModels {
+﻿namespace UBBBikeRentalSystem.Areas.Admin.ViewModels {
     public class POST_AreaAdminEditUserReservationViewModel {
         public bool isApproved { get; set; }
         public int SelectedReturnPoint { get; set; }
