45318b58240928a4b37d6835deb93e5f66ed322d
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-06-12 18:40:38 +0200

    Testy możliwości zalogowania się

--- UBBBikeRentalSystem_Tests/UnitTest1.cs
+++ UBBBikeRentalSystem_Tests/UnitTest1.cs
@@ -1,25 +1,59 @@
-namespace UBBBikeRentalSystem_Tests {
+﻿namespace UBBBikeRentalSystem_Tests {
     [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class Tests : PageTest {
         [Test]
-        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage() {
-            await Page.GotoAsync("https://playwright.dev");
+        public async Task A_UBBBikeRentalSystemIsRunning() {
+            await Page.GotoAsync("https://localhost:7265/");
 
-            // Expect a title "to contain" a substring.
-            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-            // create a locator
-            var getStarted = Page.Locator("text=Get Started");
-
-            // Expect an attribute "to be strictly equal" to the value.
-            await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-            // Click the get started link.
-            await getStarted.ClickAsync();
-
-            // Expects the URL to contain intro.
-            await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+            await Expect(Page).ToHaveTitleAsync(new Regex("UBB Bike Rental System"));
         }
+
+        [Test]
+        public async Task B_UBBBikeRentalSystemHasZalogujButton() {
+            await Page.GotoAsync("https://localhost:7265/");
+
+            var login = Page.Locator("text=Zaloguj");
+
+            await Expect(login).ToHaveAttributeAsync("href", "/Login");
+
+            await login.ClickAsync();
+
+            await Expect(Page).ToHaveTitleAsync(new Regex("Logowanie"));
+        }
+
+        [Test]
+        public async Task C_UBBBikeRentalSystemCanLogIn() {
+            await Page.GotoAsync("https://localhost:7265/");
+
+            var login = Page.Locator("text=Zaloguj");
+
+            await Expect(login).ToHaveAttributeAsync("href", "/Login");
+
+            await login.ClickAsync();
+
+            var username = Page.Locator("input[name='Input.UserName']");
+            var password = Page.Locator("input[name='Input.Password']");
+            var submit = Page.Locator("input[type='submit']");
+
+            await username.FillAsync("Administrator");
+            await password.FillAsync("Admin123!");
+            await submit.ClickAsync();
+
+            var adminpanel = Page.Locator("text=⚙️");
+            await adminpanel.ClickAsync();
+        }
+
+        [Test]
+        public async Task D_UBBBikeRentalSystemCanPlaceAReservation() { }
+
+        [Test]
+        public async Task E_UBBBikeRentalSystemCanViewAReservationInAdminArea() { }
+
+        [Test]
+        public async Task F_UBBBikeRentalSystemCanAcceptAReservationInAdminArea() { }
+
+        [Test]
+        public async Task G_F_UBBBikeRentalSystemCanFinishAReservationInAdminArea() { }
     }
 }
